8d04daab1d79907882e46ea1dedbf8d54d497ea3
Chanyoung Lee <k39335@naver.com>
2018-01-20 17:39:33 +0900

    게시글 저장, 불러오기
    게시글 저장, 불러오기 추가

--- src/main/java/com/chanyoung/mycommunity/web/WebRestController.java
+++ src/main/java/com/chanyoung/mycommunity/web/WebRestController.java
@@ -1,12 +1,25 @@
 package com.chanyoung.mycommunity.web;
 
+import com.chanyoung.mycommunity.domain.posts.PostsRepository;
+import com.chanyoung.mycommunity.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+    private PostsRepository postsRepository;
+
     @GetMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
     }
 }
