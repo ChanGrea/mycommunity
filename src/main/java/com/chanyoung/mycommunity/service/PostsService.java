1e55b81869822bdc3e55bb8bad74cac9ff327041
Chanyoung Lee <k39335@naver.com>
2018-01-22 22:33:34 +0900

    게시판 등록, 조회
    게시판 등록, 조회
    부트스트랩 추가

---
+++ src/main/java/com/chanyoung/mycommunity/service/PostsService.java
@@ -0,0 +1,29 @@
+package com.chanyoung.mycommunity.service;
+
+import com.chanyoung.mycommunity.domain.posts.PostsRepository;
+import com.chanyoung.mycommunity.dto.posts.PostsMainResponseDto;
+import com.chanyoung.mycommunity.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Service
+public class PostsService {
+    private PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto dto) {
+        return postsRepository.save(dto.toEntity()).getId();
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsMainResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
+    }
+}
